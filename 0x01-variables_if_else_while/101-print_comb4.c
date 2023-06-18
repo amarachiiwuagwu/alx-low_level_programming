779f58005e00e8a8bb6449e5eef0a0e9c5e26642
amarachiiwuagwu <iwuagwu00@gmail.com>
2023-06-18 15:03:49 -0700

    task11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,18 +9,18 @@ int main(void)
 {
 	int num, num1, num2;
 
-for (num = 48; num < 58; num++)
+for (num = 0; num < 7; num++)
 {
-for (num1 = 49; num1 < 58; num1++)
+for (num1 = 1; num1 < 8; num1++)
 {
-for (num2 = 50; num2 < 58; num2++)
+for (num2 = num1; num2 < 9; num2++)
 {
 if (num > num1 && num1 > num2)
 {
-putchar(num);
-putchar(num1);
-putchar(num2);
-if (num != 55 || num1 != 56)
+putchar('0' + num);
+putchar('0' + num1);
+putchar('0' + num2);
+if (num != 7 || num1 != 8 || num2 != 9)
 {
 putchar(',');
 putchar(' ');
